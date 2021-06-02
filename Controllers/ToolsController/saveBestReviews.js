31f206a5fb56275c1358df9ddc0b53d630cb1c48
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-06-02 08:02:07 +0200

    generating tool for best reviews

--- Controllers/ToolsController/saveBestReviews.js
+++ Controllers/ToolsController/saveBestReviews.js
@@ -5,19 +5,16 @@ const toolsResponse = require('../../ToolsResponse/response.controller');
 const mongoose = require('mongoose');
 
 module.exports.saveBestReview = (reviewBody,res) => {
-    console.log('id',reviewBody._id)
     let reviewId = reviewBody._id;
     if (!reviewId || reviewId == ''){
         console.log('here');
         reviewId = new mongoose.Types.ObjectId();
     }
     delete reviewBody._id;
-    console.log('id',reviewId);
     bestReviewModel.findByIdAndUpdate(reviewId,reviewBody,{new:true,upsert:true})
         .then((reviewUpdated) => {
-            console.log(reviewUpdated);
             if (reviewUpdated) {
-                toolsResponse.response('success',res,'BEST REVIEW UPDATED SUCCESSFULY',200);
+                toolsResponse.response('success',res,'BEST REVIEW UPDATED SUCCESSFULY',200,reviewUpdated);
             }
             else {
                 toolsResponse.response('error',res,'SOMETHING WENT WRONG !',500);
