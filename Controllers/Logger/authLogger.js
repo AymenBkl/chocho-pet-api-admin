8514382f3ad764331854383f516e3a99bdb5d254
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-06-17 01:42:30 +0200

    logger

--- Controllers/Logger/authLogger.js
+++ Controllers/Logger/authLogger.js
@@ -3,15 +3,13 @@ const authLogger = require('../../Models/auth-logger');
 module.exports.insertAuthLogger = (object) => {
     authLogger.create(object)
         .then((result) => {
-            console.log(result)
         })
         .catch(err => {
-            console.log(err);
         })
 }
 
-module.exports.getAuthLogger = (res) => {
-    authLogger.find({})
+module.exports.getAuthLogger = (res,level) => {
+    authLogger.find({level:level})
         .then((loggers) => {
             if (loggers && loggers.length > 0){
                 res.json({status:200,success:true,object:loggers});
@@ -20,11 +18,11 @@ module.exports.getAuthLogger = (res) => {
                 res.json({status:404,success:true,object:loggers});
             }
             else {
-                res.json({status:500,success:true,err:'Somthing Went Wrong !'});
+                res.json({status:500,success:false,err:'Somthing Went Wrong !'});
             }
         })
         .catch(err => {
-            res.json({status:500,success:true,err:err});
+            res.json({status:500,success:false,err:err});
         })
 }
 
--- Controllers/Logger/logger.controller.js
+++ Controllers/Logger/logger.controller.js
@@ -2,7 +2,7 @@ const authLogger = require('./authLogger');
 
 module.exports = {
     getAuthLogger : (req,res,next) => {
-        authLogger.getAuthLogger(res);
+        authLogger.getAuthLogger(res,req.query.level);
     },
 
     insertLogger : (object) => {
