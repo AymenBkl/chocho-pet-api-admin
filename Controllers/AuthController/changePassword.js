f5a4db0544f02f66aeaf1a27836760277df7c58c
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-06-16 17:05:15 +0200

    fixed login many attempts

--- Controllers/AuthController/changePassword.js
+++ Controllers/AuthController/changePassword.js
@@ -1,43 +1,73 @@
 const AdminModel = require('../../Models/admin');
 const response = require('../../UserResponse/response.controller');
+const { RateLimiterRedis } = require('rate-limiter-flexible');
+const Redis = require('ioredis');
 
+const redisClient = new Redis({
+  options: {
+    enableOfflineQueue: false,
+    
+  }
+});
+
+redisClient.set("foo", "bar");
+
+const opts = {
+  redis: redisClient,
+  points: 5, // 5 points
+  duration: 15 * 60, // Per 15 minutes
+  blockDuration: 15 * 60, // block for 15 minutes if more than points consumed 
+};
+
+const rateLimiter = new RateLimiterRedis(opts);
 module.exports.changePassword = (req,res,next) => {
-    console.log(req.user.username,req.body.oldPassword);
-    AdminModel.findByUsername(req.user.username)
-        .then((sanitizedAdmin) => {
-        console.log(sanitizedAdmin);
-        if (sanitizedAdmin){
-            sanitizedAdmin.changePassword(req.body.oldPassword,req.body.newPassword)
-                .then(updateAdmin =>{
-                    console.log('updated',updateAdmin);
-                AdminModel.updateOne({AdminId:req.body.AdminId},updateAdmin)
-                    .then((Admin) => {
-                        console.log(Admin)
-                        if (Admin){
-                            response.response("success", res, "YOUR PASSWORD HAS BEEN Changed", 200,Admin);
-                        }
-                        else {
-                          response.response("error", res, err, 404,null);
-                        }
+    rateLimiter.consume(req.connection.remoteAddress)
+        .then((data) => {
+            console.log(data.remainingPoints);
+            console.log(req.user.username,req.body.oldPassword);
+            AdminModel.findByUsername(req.user.username)
+                .then((sanitizedAdmin) => {
+                console.log(sanitizedAdmin);
+                if (sanitizedAdmin){
+                    sanitizedAdmin.changePassword(req.body.oldPassword,req.body.newPassword)
+                        .then(updateAdmin =>{
+                            console.log('updated',updateAdmin);
+                        AdminModel.updateOne({AdminId:req.body.AdminId},updateAdmin)
+                            .then((Admin) => {
+                                console.log(Admin)
+                                if (Admin){
+                                    response.response("success", res, "YOUR PASSWORD HAS BEEN Changed", 200,Admin);
+                                }
+                                else {
+                                  response.response("error", res, err, 404,null);
+                                }
+                            })
+                            .catch((err) => {
+                                console.log(err)
+                                response.response("error", res, err, 500,null);
+                            });
                     })
-                    .catch((err) => {
+                    .catch(err => {
                         console.log(err)
                         response.response("error", res, err, 500,null);
                     });
+                } else {
+                    response.response("error", res, "Admin DOSN'T EXIST", 404,null);
+                }
+            },(err) => {
+                console.log(err)
+                response.response("error", res, err, 500,null);
             })
             .catch(err => {
                 console.log(err)
                 response.response("error", res, err, 500,null);
-            });
-        } else {
-            response.response("error", res, "Admin DOSN'T EXIST", 404,null);
-        }
-    },(err) => {
-        console.log(err)
-        response.response("error", res, err, 500,null);
-    })
-    .catch(err => {
-        console.log(err)
-        response.response("error", res, err, 500,null);
-    })
+            })
+        })
+        .catch((rejRes) => {
+            // Blocked
+            const secBeforeNext = Math.ceil(rejRes.msBeforeNext / 1000) || 1;
+            res.set('Retry-After', String(secBeforeNext));
+            res.status(429).send('Too Many Requests');
+          })
+    
 }
--- Controllers/AuthController/login.js
+++ Controllers/AuthController/login.js
@@ -3,27 +3,57 @@ const jwt = require('../../Middlewares/jwt/jwt');
 const response = require('../../UserResponse/response.controller');
 var  passport = require("passport");
 const admin = require("../../Models/admin");
+const { RateLimiterRedis } = require('rate-limiter-flexible');
+const Redis = require('ioredis');
 
+const redisClient = new Redis({
+  options: {
+    enableOfflineQueue: false,
+    
+  }
+});
+
+redisClient.set("foo", "bar");
+
+const opts = {
+  redis: redisClient,
+  points: 5, // 5 points
+  duration: 15 * 60, // Per 15 minutes
+  blockDuration: 15 * 60, // block for 15 minutes if more than points consumed 
+};
+
+const rateLimiter = new RateLimiterRedis(opts);
 module.exports = {
     login: (req, res, next) => {
-        passport.authenticate('local',{session: false}, (err, admin, info) => {
-            console.log(admin,info);
-            if (!admin) {
-                response.response("error", res, info, 401,null);
-                next();
-            }
-            else {
-                req.login(admin, (err) => { 
-                    if (err) {
-                        console.log('err',err)
-                        response.response("error", res, err, 401,null);
+        rateLimiter.consume(req.connection.remoteAddress)
+            .then(data => {
+                console.log(data);
+                console.log(data.remainingPoints);
+                passport.authenticate('local',{session: false}, (err, admin, info) => {
+                    if (!admin) {
+                        response.response("error", res, info, 401,null);
+                        next();
                     }
-                    
-                    const token = jwt.getToken({ _id: admin._id });
-                    response.response("success", res, token, 200,admin);
-                })
-            }
-                
-            })(req, res, next)
+                    else {
+                        req.login(admin, (err) => { 
+                            if (err) {
+                                console.log('err',err)
+                                response.response("error", res, err, 401,null);
+                            }
+                            
+                            const token = jwt.getToken({ _id: admin._id });
+                            response.response("success", res, token, 200,admin);
+                        })
+                    }
+                        
+                    })(req, res, next)
+        })
+        .catch((rejRes) => {
+            // Blocked
+            const secBeforeNext = Math.ceil(rejRes.msBeforeNext / 1000) || 1;
+            res.set('Retry-After', String(secBeforeNext));
+            res.status(429).send('Too Many Requests');
+          })
+        
     }
 }
